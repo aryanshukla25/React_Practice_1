d1483fc6352bcf9ea5970c62b955e5c0555ba7c9
Aryan Shukla <aryanshukla25@gmail.com>
2024-06-29 23:40:42 +0530

    React proj 1

---
+++ project-1/src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from './components/Navigation'
+import HeroSection from './components/HeroSection.jsx'
+import './App.css'
+const App = () => {
+    return (
+    <div> 
+        <Navigation/>
+        <HeroSection/>
+
+        
+    </div>
+    )
+};
+
+export default App;
